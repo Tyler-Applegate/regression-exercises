1315dee001e1cf3128152ec2a2d386a9008bfcce
Tyler-Applegate <tyler.applegate@gmail.com>
2021-06-07 12:09:35 -0500

    lunchtime saving point

--- wrangle.py
+++ wrangle.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import os
 from env import host, user, password
+from sklearn.model_selection import train_test_split
 
 # sets up a secure connection to the Codeup db using my login info
 
@@ -101,3 +102,59 @@ def get_zillow_data():
         df.to_csv('zillow_2017.csv')
 
     return df    
+
+def split_data(df):
+    '''
+    take in a DataFrame and return train, validate, and test DataFrames; stratify on survived.
+    return train, validate, test DataFrames.
+    '''
+    train_validate, test = train_test_split(df, test_size=.2, random_state=1221)
+    train, validate = train_test_split(train_validate, 
+                                       test_size=.3, 
+                                       random_state=1221)
+    return train, validate, test
+
+def wrangle_grades():
+    '''
+    Read student_grades csv file into a pandas DataFrame,
+    drop student_id column, replace whitespaces with NaN values,
+    drop any rows with Null values, convert all columns to int64,
+    return cleaned student grades DataFrame.
+    '''
+    # Acquire data from csv file.
+    grades = pd.read_csv('student_grades.csv', error_bad_lines=False)
+    
+    # Replace white space values with NaN values.
+    grades = grades.replace(r'^\s*$', np.nan, regex=True)
+    
+    # Drop all rows with NaN values.
+    df = grades.dropna()
+    
+    # Convert all columns to int64 data types.
+    df = df.astype('int')
+    
+    return df
+
+
+# Generic splitting function for continuous target.
+
+def split_continuous(df):
+    '''
+    Takes in a df
+    Returns train, validate, and test DataFrames
+    '''
+    # Create train_validate and test datasets
+    train_validate, test = train_test_split(df, 
+                                        test_size=.2, 
+                                        random_state=1221)
+    # Create train and validate datsets
+    train, validate = train_test_split(train_validate, 
+                                   test_size=.3, 
+                                   random_state=1221)
+
+    # Take a look at your split datasets
+
+    print(f'train -> {train.shape}')
+    print(f'validate -> {validate.shape}')
+    print(f'test -> {test.shape}')
+    return train, validate, test
